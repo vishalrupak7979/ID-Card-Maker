bd47a2ab91276858a679bfbf99d3637ab8e5cabc
vishalrupak7979 <vishalrupakvr@gmail.com>
2025-04-15 14:52:33 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold text-center mb-4 text-blue-700">
-          Welcome to the ID Card Maker
-        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust paths based on your project structure
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
