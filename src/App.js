de44d8c937e0eae8bbf850edc43fd7dd7c93da3b
vishalrupak7979 <vishalrupakvr@gmail.com>
2025-04-15 14:19:59 +0530

    installing tailwind and bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold text-center mb-4 text-blue-700">
+          Welcome to the ID Card Maker
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust paths based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
