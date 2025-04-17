156dc2f50b3f07195e6e0d3aaf1b444a5fecf326
vishalrupak7979 <vishalrupakvr@gmail.com>
2025-04-17 11:15:00 +0530

    deployment bug fixing

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 const ConfirmationModal = ({ isOpen, onClose, onConfirm, message }) => {
--- src/components/TopMenuBar.jsx
+++ src/components/TopMenuBar.jsx
@@ -31,13 +31,13 @@ const TopMenuBar = () => {
     <div className="flex flex-col items-center pt-2 pl-4 pr-4 pb-2 border-b border-gray-700 bg-[#111827]">
       {/* Centered Menu */}
       <div className="flex space-x-6 text-sm md:text-base font-medium mb-2 items-center">
-        <a href="#" className="hover:text-blue-400">Home</a>
-        <a href="#" className="hover:text-blue-400">Dashboard</a>
+        <a href="/" className="hover:text-blue-400">Home</a>
+        <a href="/" className="hover:text-blue-400">Dashboard</a>
         <div className="flex items-center space-x-1">
-          <a href="#" className="hover:text-blue-400">Modules</a>
+          <a href="/" className="hover:text-blue-400">Modules</a>
           <Plus size={16} className="text-white hover:text-blue-400 cursor-pointer" />
         </div>
-        <a href="#" className="hover:text-blue-400">Map</a>
+        <a href="/" className="hover:text-blue-400">Map</a>
       </div>
       {/* Date and Time */}
       <div className="flex items-center text-xs md:text-sm text-gray-400">
